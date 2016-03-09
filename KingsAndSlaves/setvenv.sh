da2c3edbaf02df1dc428a9af47f48421511cd4ef
Trofimov Igor <mister.larpy@ya.ru>
2016-03-09 17:32:19 +0500

    Вот и первое задание.
    Проблема:
      Наличие борьбы за аккаунт Бога.
    PS:
      После определения тематики qctf подправим легенду игры.
    
    Signed-off-by: Trofimov Igor <mister.larpy@ya.ru>

---
+++ KingsAndSlaves/setvenv.sh
@@ -0,0 +1,6 @@
+# Нужен python3, virtualenv для python3
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+# Для запуска вначале запускаем виртуальное окружение 'source venv/bin/activate'
+# Затем запускаем сам таск 'python3 main.py'
