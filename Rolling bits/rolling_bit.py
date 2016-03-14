ce72765556adef7dc584db329a2758e3829bef6d
pyrolynx <briizzzz@mail.ru>
2016-03-15 00:45:49 +0500

    Initial commit

---
+++ Rolling bits/rolling_bit.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from socket import socket
+
+
+def main():
+    IP = ""
+    PORT = 8800
+    ADDR = (IP,PORT)
+    sock = socket()
+    sock.bind(ADDR)
+    while 1:
+        data = sock.recvfrom(65545)
+        if not data:
+            break
+        print(transponate(data))
+
+
+def transonate(data):
+    transponData = []
+    for i in range(0,len(data),8):
+        octet = data[i:i+8] + b"\x00\x00\x00\x00\x00\x00\x00\x00"[:8-len(data[i:i+8])]
+        bytematrix = generate_binary_matrix(octet)
+        # print_matrix(bytematrix)
+        transponMatrix = []
+        for y in range(8):
+            transponMatrix.append([])
+            for x in range(8):
+                transponMatrix[y].append(bytematrix[7-x][y])
+        byteArr = []
+        for i in range(8):
+            charbits = []
+            for j in range(8):
+                charbits.append(transponMatrix[j][i])
+            byteArr.append(chr(int(''.join(charbits),2)))
+        result = ''.join(byteArr)
+        transponData.append(result)
+        print(len(result))
+    return ''.join(transponData).encode()
+        
+
+def print_matrix(matrix):
+    for x in range(8):
+        for y in range(8):
+            print(matrix[x][y],end=' ')
+        print('')
+    print('')
+
+
+def generate_binary_matrix(bytestring):
+    bytemap = []
+    for i in range(8):
+        bytemap.append([])
+        for j in range(8):
+            bytemap[i].append('')
+    for i in range(len(bytestring)):
+        bitstring = "00000000"[:8-len(bin(bytestring[i])[2:])] + bin(bytestring[i])[2:]
+        for j in range(len(bitstring)):
+            bytemap[j][i]=bitstring[j]
+    # for i in bytemap:
+    #     print(i)
+    return bytemap
+
+if __name__ == '__main__':
+    main()
