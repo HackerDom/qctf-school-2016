7e9ac3f45370a045fad0c8dc7f0daacd6d5c4662
Learko <matveevantonekb@gmail.com>
2016-03-20 18:03:13 +0500

    pep8

--- Eval/server.py
+++ Eval/server.py
@@ -4,10 +4,10 @@ sock = socket.socket()
 sock.bind(('', 1337))
 sock.listen(5)
 
-while 1:
-	conn, addr = sock.accept()
-	data = conn.recv(1024)
+while True:
+    conn, addr = sock.accept()
+    data = conn.recv(1024)
 
-	res = str(eval(data))
-	conn.send(res.encode('utf8'))
-	conn.close()
+    res = str(eval(data))
+    conn.send(res.encode('utf8'))
+    conn.close()
