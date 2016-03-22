bae7ad80f80e833de5f433195160dd2f26ac2b9a
a1exdandy <darkjedi.alex@yandex.ru>
2016-03-22 11:58:59 +0500

    add encoder building to build.sh

--- Hamming/build.sh
+++ Hamming/build.sh
@@ -8,7 +8,17 @@ fi
 echo "Start building"
 echo
 
-echo " 1) Prepare image (add description text)"
+echo " 1) Build encoder"
+make -C ./tools/encoder/ 1>/dev/null
+if [ $? -ne 0 ]; then
+	echo "[-] Fail"
+	exit
+else
+	echo "[+] Success"
+fi
+
+
+echo " 2) Prepare image (add description text)"
 ./tools/prepare_image.py $1 target_image.bmp
 if [ $? -ne 0 ]; then
 	echo "[-] Fail"
@@ -22,7 +32,7 @@ flag=QCTF_$md5_hash
 echo $flag > flag.txt
 echo "[!] Flag is" $flag "(see flag.txt file)"
 
-echo " 2) Encode image with Hamming-(15, 11)"
+echo " 3) Encode image with Hamming-(15, 11)"
 ./tools/encoder/encoder target_image.bmp original_message.bin
 if [ $? -ne 0 ]; then
 	echo "[-] Fail"
@@ -31,7 +41,7 @@ else
 	echo "[+] Success"
 fi
 
-echo " 3) Corrupt encoded image"
+echo " 4) Corrupt encoded image"
 ./tools/corrupt_message.py original_message.bin message.bin
 if [ $? -ne 0 ]; then
 	echo "[-] Fail"
@@ -40,7 +50,7 @@ else
 	echo "[+] Success"
 fi
 
-echo " 4) Check corrupted image"
+echo " 5) Check corrupted image"
 solution=$(./tools/check.py message.bin)
 if [[ "$flag" != "$solution" ]]; then
 	echo "[-] Different flags!"
