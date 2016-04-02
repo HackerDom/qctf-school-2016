faf0f7023b36c7abc77b8d5aa455de5aa306774a
Andrew Gein <andgein@yandex.ru>
2016-04-02 21:48:26 +0500

    Platform: Checking flags

--- platform/web/send_flag.php
+++ platform/web/send_flag.php
@@ -1,4 +1,5 @@
 <?php
+
     require_once 'session.php';
     require_once 'db.php';
 
@@ -8,7 +9,14 @@
         exit;
     }
 
+    if (! array_key_exists('flag', $_GET))
+    {
+        echo json_encode(['status' => 'failed', 'message' => 'Specify the flag']);
+        exit;
+    }
+
     $task_id = (int) $_GET['task_id'];
+    $flag = $_GET['flag'];
 
     if ($_SESSION['user_id'] === false)
     {
@@ -24,6 +32,8 @@
         exit;
     }
 
-    # TODO: read task from file by its name
-    echo json_encode(['status' => 'ok', 'task' => ['title' => 'Hello world', 'html' => '<p>Это пробное задание</p>']]);
+    $is_correct = $task['flag'] == $flag;
+    create_submission($_SESSION['user_id'], $task_id, $flag, $is_correct);
+
+    return json_encode(['status' => 'ok', 'is_correct' => $is_correct]);    
 ?>
